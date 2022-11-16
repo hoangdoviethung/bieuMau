c9a7f9fabf2de92a82f96279e6422f0924bf7570
hung.hoang <hung.hoang@bmbsoft.com.vn>
2022-11-16 17:22:08 +0700

    .

--- src/main/java/com/rest/Transsfer.java
+++ src/main/java/com/rest/Transsfer.java
@@ -34,4 +34,65 @@ public class Transsfer {
         headers.setContentType(mediaType);
         return ResponseEntity.ok().headers(headers).body(byteArr);
     }
+    /*
+    *
+    *
+    *
+    *
+    * [
+    {
+        "soDG": "SEATELLER20_FEERE_19102022_1668847461",
+        "ngayGio": "19-10-2022 15:52:08",
+        "user": "SEATELLER20",
+        "bangKe": "",
+        "tenKhachHang": "TRAN THI YEN",
+        "gttt": "163042021",
+        "noiDung": "The replaceAll() method doesn't change the contents of the original string, it returns a new string with all matches replaced.",
+        "tongSoTienChi": "100000",
+        "bangChu": "",
+        "ngayCap": "27/10/2020",
+        "maKH": "14892607",
+        "noiCap": "CA NAM DINH dfdfdfdf",
+        "sdt": "0855254514",
+        "diaChi": "22 ngo 210, doi can KHI KI VAM KIM LU HAI BAN TRUNG HF, QUAN BA DINH, HA NOI CITY",
+        "nguoiNhanTien": "TRAN THI YEN",
+        "nguoiNhapLieu": "NGUYEN THI THANH PHUONG",
+        "nguoiKiemSoat": "",
+        "nguoiPheDuyet": "",
+        "coCode": "Chi nhánh Sở giao dịch (VN0010002)",
+        "type": "VND",
+        "tongCong": "100000"
+    },
+    {
+        "soDG": "SEATELLER20_FEERE_19102022_1668847461",
+        "ngayGio": "19-10-2022 15:52:08",
+        "user": "SEATELLER20",
+        "bangKe": "",
+        "tenKhachHang": "TRAN THI YEN",
+        "gttt": "163042021",
+        "noiDung": "note",
+        "tongSoTienChi": "100000",
+        "bangChu": "",
+        "ngayCap": "27/10/2020",
+        "maKH": "14892607",
+        "noiCap": "CA NAM DINH dfdfdfdf",
+        "sdt": "0855254514",
+        "diaChi": "22 ngo 210, doi can KHI KI VAM KIM1 LU HAI BAN TRUNG HF",
+        "nguoiNhanTien": "TRAN THI YEN",
+        "nguoiNhapLieu": "NGUYEN THI THANH PHUONG",
+        "nguoiKiemSoat": "",
+        "nguoiPheDuyet": "",
+        "coCode": "Chi nhánh Sở giao dịch (VN0010002)",
+        "loaiBangKe": "Bảng kê các loại tiền lĩnh (VND)",
+        "type": "VND",
+        "tongCong": "100000",
+        "fields": [
+            {
+                "menhGia": 20000,
+                "soTo": 5,
+                "thanhTien": 100000
+            }
+        ]
+    }
+]*/
 }
